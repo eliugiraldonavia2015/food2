84af95eab4cfebc3b24d31650fd7ce4a4d1c3903
eliu giraldo <eliugiraldonavia2015@gmail.com>
2025-12-06 21:56:40 -0500

    messages screen design 3

--- food/Sources/Views/MessagesView.swift
+++ food/Sources/Views/MessagesView.swift
@@ -233,15 +233,6 @@ struct MessageBubble: View {
     }
 }
 
-struct RoundedCorner: Shape {
-    var radius: CGFloat = 16
-    var corners: UIRectCorner = [.allCorners]
-
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
 
 struct Conversation: Identifiable, Hashable {
     let id = UUID()
