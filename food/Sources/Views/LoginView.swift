895d2e257171c7f6429dfca71f4029225dc2dc6c
eliugiraldonavia2015 <eliugiraldonavia2015@gmail.com>
2026-02-12 16:03:59 -0500

    login screen edit

--- food/Sources/Views/LoginView.swift
+++ food/Sources/Views/LoginView.swift
@@ -537,15 +537,7 @@ struct SocialIconBtn: View {
     }
 }
 
-struct RoundedCorner: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
+
 
 // MARK: - Success Animation
 struct SuccessExplosionView: View {
